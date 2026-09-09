4913913a67b1df35d6ca73f424d6af807715ca08
Shaban-thc <Shabanali.Shaikh@Thc-cons.com>
2026-09-09 19:37:20 +0530

    first commit

---
+++ src/config/env.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3000),
+
+  DATABASE_URL: z.string().min(1),
+
+  JWT_ACCESS_SECRET: z.string().min(32),
+  JWT_ACCESS_EXPIRES_IN: z.string(),
+
+  JWT_REFRESH_SECRET: z.string().min(32),
+  JWT_REFRESH_EXPIRES_IN: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
